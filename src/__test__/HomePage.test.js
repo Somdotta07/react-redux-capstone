0d0c0152f73e8ad71ddb8a26607126cbe818329d
Somdotta07 <jhilick.s@gmail.com>
2021-12-21 12:22:25 +0100

    Add tests for the pages

---
+++ src/__test__/HomePage.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../Redux/configureStore';
+import HomePage from '../Components/HomePage';
+
+describe('Render components', () => {
+  test('render HomePage', () => {
+    const tree = renderer
+      .create(<Router><Provider store={store}><HomePage /></Provider></Router>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
